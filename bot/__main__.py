b1035cacb899009477fc1111ba9254953c7eb578
dikshitdd <95383028+dikshitdd@users.noreply.github.com>
2023-04-09 12:13:56 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -9,5 +9,30 @@ if __name__ == "__main__":
     logging.getLogger("pyrogram").setLevel(
         logging.INFO if Config.DEBUG else logging.WARNING
     )
+    
+    # Define a function to handle the /tags command
+def tags(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! Send me a YouTube video URL and I'll fetch its tags.")
+
+# Define a function to handle incoming messages
+def handle_message(update, context):
+    url = update.message.text
+    tags = YouTubeTags.get_tags(url)
+    if tags:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Tags: {}".format(", ".join(tags)))
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I couldn't fetch the tags. Please check if the URL is correct.")
+
+# Create a Telegram bot object and set up a CommandHandler
+bot_token = Config.BOT_TOKEN,
+bot = telegram.Bot(token=bot_token)
+updater = Updater(token=bot_token, use_context=True)
+dispatcher = updater.dispatcher
+start_handler = CommandHandler('tags', tags)
+dispatcher.add_handler(start_handler)
+
+# Set up a message handler
+message_handler = telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message)
+dispatcher.add_handler(message_handler)
 
     UtubeBot().run()
