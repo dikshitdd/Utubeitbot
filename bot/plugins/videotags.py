c3632303d5a6a2cac74575021a025fb34a1766ec
dikshitdd <95383028+dikshitdd@users.noreply.github.com>
2023-04-09 11:48:09 +0530

    Update videotags.py

--- bot/plugins/videotags.py
+++ bot/plugins/videotags.py
@@ -1,6 +1,9 @@
 import telegram
+import logging
 from telegram.ext import Updater, CommandHandler
 from youtubetags import YouTubeTags
+from .utubebot import UtubeBot
+from .config import Config
 
 # Define a function to handle the /tags command
 def tags(update, context):
@@ -16,11 +19,11 @@ def handle_message(update, context):
         context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I couldn't fetch the tags. Please check if the URL is correct.")
 
 # Create a Telegram bot object and set up a CommandHandler
-bot_token = "YOUR_BOT_TOKEN_HERE"
+bot_token = Config.BOT_TOKEN,
 bot = telegram.Bot(token=bot_token)
 updater = Updater(token=bot_token, use_context=True)
 dispatcher = updater.dispatcher
-start_handler = CommandHandler('start', start)
+start_handler = CommandHandler('tags', tags)
 dispatcher.add_handler(start_handler)
 
 # Set up a message handler
